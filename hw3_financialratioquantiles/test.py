ae3b2a748d55f502b34bfdbb7569ceea488b48ba
MrHumble-Bee <alexhuang.e@gmail.com>
2024-02-01 21:47:54 -0600

    DMV Strat

---
+++ hw3_financialratioquantiles/test.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# Example arrays
+array1 = np.array([1, 2, 3, 4, 5])
+array2 = np.array([10, 20, 30, 40, 50])
+
+# Scale each element in array1 by the corresponding element in array2
+scaled_result = array1 * array2
+
+# Display the result
+print(scaled_result)
